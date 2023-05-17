6e2b2dfdc0c5a71860ebc93481bf7340757bbb56
bryandk26 <bryandarmawa13@gmail.com>
2023-05-17 13:57:09 +0700

    Add StoreScreen with the detail

--- lib/buyers/views/nav_screens/store_screen.dart
+++ lib/buyers/views/nav_screens/store_screen.dart
@@ -1,12 +1,110 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:second_chance/buyers/views/nav_screens/widget_screen/store_detail_screen.dart';
+import 'package:second_chance/theme.dart';
 
-class StoreScreen extends StatelessWidget {
-  const StoreScreen({super.key});
+class StoreScreen extends StatefulWidget {
+  const StoreScreen({Key? key});
+
+  @override
+  State<StoreScreen> createState() => _StoreScreenState();
+}
+
+class _StoreScreenState extends State<StoreScreen> {
+  final Stream<QuerySnapshot> _vendorsStream =
+      FirebaseFirestore.instance.collection('vendors').snapshots();
+
+  String? _searchValue = '';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Store Screen')),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: whiteColor,
+        centerTitle: true,
+        elevation: 0,
+        title: TextFormField(
+          onChanged: (value) {
+            setState(() {
+              _searchValue = value;
+            });
+          },
+          decoration: InputDecoration(
+            labelText: 'Search Store...',
+            labelStyle: TextStyle(
+              color: blackColor,
+            ),
+            prefixIcon: Icon(
+              Icons.search,
+              color: blackColor,
+            ),
+          ),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _vendorsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final searchedData = snapshot.data!.docs.where((element) {
+            return element['businessName']
+                .toLowerCase()
+                .contains(_searchValue?.toLowerCase() ?? '');
+          }).toList();
+
+          if (searchedData.isEmpty) {
+            return Center(
+              child: Text(
+                'Searched Store is Not Found',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 5,
+                ),
+              ),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: searchedData.length,
+            itemBuilder: (context, index) {
+              final storeData = searchedData[index];
+              final storeImage = storeData['storeImage'];
+              return InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return StoreDetailScreen(
+                        storeData: storeData,
+                      );
+                    },
+                  ));
+                },
+                child: Card(
+                  child: ListTile(
+                    title: Text(storeData['businessName']),
+                    subtitle: Text(storeData['countryValue']),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        storeImage == null || storeImage == ''
+                            ? 'https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg'
+                            : storeData['storeImage'],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
