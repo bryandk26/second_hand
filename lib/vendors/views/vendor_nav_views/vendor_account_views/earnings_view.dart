b87fdb8eada33fba7c904576f534242158852f99
bryandk26 <bryandarmawa13@gmail.com>
2023-06-13 01:06:50 +0700

    refactor function for earnings view

--- lib/vendors/views/vendor_nav_views/vendor_account_views/earnings_view.dart
+++ lib/vendors/views/vendor_nav_views/vendor_account_views/earnings_view.dart
@@ -6,7 +6,15 @@ import 'package:second_chance/theme.dart';
 import 'package:intl/intl.dart';
 
 class EarningsView extends StatelessWidget {
-  const EarningsView({super.key});
+  const EarningsView({Key? key});
+
+  Future<double> calculateTotalEarnings(QuerySnapshot querySnapshot) async {
+    double totalEarnings = 0.0;
+    for (var orderItem in querySnapshot.docs) {
+      totalEarnings += orderItem['productPrice'];
+    }
+    return totalEarnings;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,124 +44,139 @@ class EarningsView extends StatelessWidget {
               snapshot.data!.data() as Map<String, dynamic>;
 
           return Scaffold(
-              appBar: AppBar(
-                backgroundColor: whiteColor,
-                elevation: 0,
-                centerTitle: true,
-                leading: IconButton(
-                  icon: Icon(CupertinoIcons.back),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                iconTheme: IconThemeData(color: blackColor),
-                title: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(data['storeImage']),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(data['businessName'], style: subTitle),
-                    ),
-                  ],
-                ),
+            appBar: AppBar(
+              backgroundColor: whiteColor,
+              elevation: 0,
+              centerTitle: true,
+              leading: IconButton(
+                icon: Icon(CupertinoIcons.back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-              body: StreamBuilder<QuerySnapshot>(
-                stream: _ordersStream,
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return Center(child: Text('Something went wrong'));
-                  }
+              iconTheme: IconThemeData(color: blackColor),
+              title: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(data['storeImage']),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(data['businessName'], style: subTitle),
+                  ),
+                ],
+              ),
+            ),
+            body: StreamBuilder<QuerySnapshot>(
+              stream: _ordersStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text('Something went wrong'));
+                }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        color: blackColor,
-                      ),
-                    );
-                  }
-
-                  double totalEarnings = 0.0;
-                  for (var orderItem in snapshot.data!.docs) {
-                    totalEarnings += orderItem['productPrice'];
-                  }
-
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            height: 150,
-                            width: MediaQuery.of(context).size.width * 0.5,
-                            decoration: BoxDecoration(
-                              color: blackColor,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    'TOTAL EARNINGS',
-                                    style: textButton,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    '${NumberFormat.currency(locale: 'id', symbol: 'Rp ').format(totalEarnings)}',
-                                    style: TextStyle(
-                                      color: Colors.green,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 150,
-                            width: MediaQuery.of(context).size.width * 0.5,
-                            decoration: BoxDecoration(
-                              color: blackColor,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    'TOTAL ORDERS',
-                                    style: textButton,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    snapshot.data!.docs.length.toString(),
-                                    style: TextStyle(
-                                      color: whiteColor,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                    child: CircularProgressIndicator(
+                      color: blackColor,
                     ),
                   );
-                },
-              ));
+                }
+
+                return FutureBuilder<double>(
+                  future: calculateTotalEarnings(snapshot.data!),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<double> earningsSnapshot) {
+                    if (earningsSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: blackColor,
+                        ),
+                      );
+                    }
+
+                    double totalEarnings = earningsSnapshot.data ?? 0.0;
+
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: 150,
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              decoration: BoxDecoration(
+                                color: blackColor,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      'TOTAL EARNINGS',
+                                      style: textButton,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      '${NumberFormat.currency(locale: 'id', symbol: 'Rp ').format(totalEarnings)}',
+                                      style: TextStyle(
+                                        color: Colors.green,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              height: 150,
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              decoration: BoxDecoration(
+                                color: blackColor,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      'TOTAL ORDERS',
+                                      style: textButton,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      snapshot.data!.docs.length.toString(),
+                                      style: TextStyle(
+                                        color: whiteColor,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          );
         }
         return Center(
             child: CircularProgressIndicator(
